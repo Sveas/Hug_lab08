984fa4ea721312079d68fa691105eabaf0243d6f
Voidnoob <sveinn@ismenn.net>
2018-10-10 15:27:26 +0000

    baetti vid api og setti upp circleci

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const greeting = require('./src/greeting');
+//const greeting = require('./src/greeting');
+//console.log(greeting('Totoro'));
 
-console.log(greeting('Totoro'));
-
+const app = require("./src/api");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
